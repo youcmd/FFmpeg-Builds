187e17842459104dd34c24166314c9638f5afee5
youcmd <128134779+youcmd@users.noreply.github.com>
2026-09-13 11:28:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -35,6 +35,9 @@ cat <<EOF >"$BUILD_SCRIPT"
     git clone --filter=blob:none --branch='$GIT_BRANCH' '$FFMPEG_REPO' ffmpeg
     cd ffmpeg
 
+    sed -i '/void show_banner(/,/^}/ { /void show_banner(/! { /}/! d } }' fftools/cmdutils.c
+    sed -i '/void show_banner(/a { return; }' fftools/cmdutils.c
+
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS \$FF_CONFIGURE \
         --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
         --extra-ldflags="\$FF_LDFLAGS" --extra-ldexeflags="\$FF_LDEXEFLAGS" \
